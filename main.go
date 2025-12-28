de7406f8ba8aab23c0d293fc3c714211bb1be187
Paari Laishram <paarilai05@gmail.com>
2025-12-28 19:48:14 +0530

    changes paari

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Changes has been made")
+	fmt.Println("Paari has made changes")
 }
 
 func CalculateTotal(item_count int, item_price float64) float64 {
-	return float64(item_count) + item_price
+	return float64(item_count) * item_price
 }
